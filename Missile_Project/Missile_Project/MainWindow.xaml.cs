b487243fd58d78b076c7ad7ea852efe1bdab3efa
mosheco110 <mosheco110@gmail.com>
2018-12-27 13:26:12 +0200

    v

--- Missile_Project/Missile_Project/MainWindow.xaml.cs
+++ Missile_Project/Missile_Project/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExifLib;
+using ExifReader = ExifLib.ExifReader;
 
 namespace Missile_Project
 {
@@ -26,25 +28,67 @@ namespace Missile_Project
         public MainWindow()
         {
             InitializeComponent();
-            GetGeoCoordinate("IL_Jerusalem_Havad_haleumi");
-            GetGeoCoordinateExif();
+           // GetGeoCoordinate("IL_Jerusalem_Havad_haleumi");
+           // GetGeoCoordinateExif();
+            //string imagepath = @"C:\Users\Moshe\Desktop\exif-samples-master\exif-samples-master\jpg\gps\DSCN0012.jpg";
+            string imagepath = @"C:\Users\Moshe\Desktop\exif2.jpg";
+           
+            GetLatLongFromImage(imagepath);
         }
         private async void GetGeoCoordinate(string address)
         {
-           var Coordinate = await ViewModels.GeoCoordinateApi.GetGeoCoordinateAsync(address);
+            var Coordinate = await ViewModels.GeoCoordinateApi.GetGeoCoordinateAsync(address);
             double let = Coordinate.results[0].geometry.location.lat;
             double lng = Coordinate.results[0].geometry.location.lng;
+        }
 
-           
+        public static double[] GetLatLongFromImage(string imagePath)
+        {
+            ExifReader reader = new ExifReader(imagePath);
+
+            // EXIF lat/long tags stored as [Degree, Minute, Second]
+            double[] latitudeComponents;
+            double[] longitudeComponents;
+
+            string latitudeRef; // "N" or "S" ("S" will be negative latitude)
+            string longitudeRef; // "E" or "W" ("W" will be a negative longitude)
+
+            if (reader.GetTagValue(ExifTags.GPSLatitude, out latitudeComponents)
+                && reader.GetTagValue(ExifTags.GPSLongitude, out longitudeComponents)
+                && reader.GetTagValue(ExifTags.GPSLatitudeRef, out latitudeRef)
+                && reader.GetTagValue(ExifTags.GPSLongitudeRef, out longitudeRef))
+            {
+
+                var latitude = ConvertDegreeAngleToDouble(latitudeComponents[0], latitudeComponents[1], latitudeComponents[2], latitudeRef);
+                var longitude = ConvertDegreeAngleToDouble(longitudeComponents[0], longitudeComponents[1], longitudeComponents[2], longitudeRef);
+                return new[] { latitude, longitude };
+            }
+
+            return null;
 
         }
-        private  void GetGeoCoordinateExif()
+        public static double ConvertDegreeAngleToDouble(double degrees, double minutes, double seconds, string latLongRef)
         {
-            //string imagepath = @"C:\Users\Moshe\source\repos\Missile_Project_1\Missile_Project\Missile_Project\images\dcv.jpg";
+            double result = ConvertDegreeAngleToDouble(degrees, minutes, seconds);
+            if (latLongRef == "S" || latLongRef == "W")
+            {
+                result *= -1;
+            }
+            return result;
+        }
+
+        public static double ConvertDegreeAngleToDouble(double degrees, double minutes, double seconds)
+        {
+            return degrees + (minutes / 60) + (seconds / 3600);
+        }
+        private void GetGeoCoordinateExif()
+        {
+            /*
+           / //string imagepath = @"C:\Users\Moshe\source\repos\Missile_Project_1\Missile_Project\Missile_Project\images\dcv.jpg";
             //string imagepath = @"C:\Users\Moshe\Desktop\img_2158.jpg";
             //string imagepath = @"C:\Users\Moshe\Desktop\exif-samples-master\exif-samples-master\jpg\exif-org\canon-ixus.jpg";
             string imagepath = @"C:\Users\Moshe\source\repos\Missile_Project_1\Missile_Project\Missile_Project\images\olympus-d320l.jpg";
-
+           string imagepath = @" C:\Users\Moshe\Desktop\exif-samples-master\exif-samples-master\jpg\gps\DSCN0012.jpg
             var directories = ImageMetadataReader.ReadMetadata(imagepath);
 
             // print out all metadata
@@ -68,7 +112,7 @@ namespace Missile_Project
         //    var location = gps.GetGeoLocation();
 
         //    //Console.WriteLine("Image at {0},{1}", location.Latitude, location.Longitude);
-          
+          */
         }
 
         private void ButtonClikP1(object sender, RoutedEventArgs e)
